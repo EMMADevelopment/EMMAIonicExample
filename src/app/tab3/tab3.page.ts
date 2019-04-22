3226021bc0071ff60c645c8c14ad1741c0f05a9e
Adrian Carrera <adrian@emma.io>
2019-04-22 08:51:36 +0200

    testing methods

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,58 @@
 import { Component } from '@angular/core';
 
+declare var window: any;
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+
+  ionViewDidEnter() {
+    console.log('DID ENTER');
+    const EMMA = window.plugins.EMMA;
+
+    const message = {
+      type: 'nativeAd',
+    };
+
+    EMMA.trackUserExtraInfo(message);
+
+
+    const eventRequest = {
+      token: 'token'
+    };
+
+    EMMA.trackEvent(eventRequest);
+
+    const loginRegister = {
+      userId: 'adri',
+      email: 'adrian@emma.io'
+    };
+
+    EMMA.registerUser(loginRegister);
+    EMMA.loginUser(loginRegister);
+
+
+    const order = {
+      orderId: 'L-X98421',
+      totalPrice: 23.4,
+      currencyCode: 'EUR'
+    };
+
+    EMMA.startOrder(order);
+
+    const product = {
+      productId: 'PROD-ORDER1',
+      productName: 'Chaqueta-cuadros',
+      quantity: 1,
+      price: 23.4
+    };
+
+    EMMA.addProduct(product);
+
+    EMMA.trackOrder();
+  }
+
+}
