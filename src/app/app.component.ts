e0a254ff7094205791813e77a530606a51d2d523
Adrian Carrera <adrian@emma.io>
2019-04-24 01:47:14 +0200

    testing deeplink with alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -14,29 +14,35 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private alertCtrl: AlertController
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      this.initEMMA();
       this.statusBar.styleDefault();
       this.splashScreen.hide();
-      this.initEMMA();
     });
   }
 
   initEMMA() {
 
+    document.addEventListener('onDeepLink', (event) => {
+      const msg = JSON.stringify(event);
+      this.presentDeepLinkAlert(msg);
+    });
+
     const EMMA = window.plugins.EMMA;
 
     const configuration = {
-        sessionKey: 'emmaionicoJxuVt2o3',
-        debug: true
+      sessionKey: 'emmaionicoJxuVt2o3',
+      debug: true
     };
 
-   EMMA.startSession(configuration);
+    EMMA.startSession(configuration);
 
     const pushOptions = {
       classToOpen: 'io.emma.cordova.exampleionic.MainActivity',
@@ -44,5 +50,15 @@ export class AppComponent {
     };
 
     EMMA.startPush(pushOptions);
+  }
+
+  presentDeepLinkAlert(msg) {
+    this.alertCtrl.create({
+      header: 'DeepLink',
+      message: msg,
+      buttons: ['OK']
+    }).then((alert) => {
+      alert.present();
+    });
   }
 }
