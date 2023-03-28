57075fabcef1f19bddf3611d43a0beae8957682f
Adrian Carrera <adrian@emma.io>
2023-03-28 11:55:46 +0200

    Changes on ios folder

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,6 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports = [
     {
+      "id": "cordova-plugin-emma-sdk.emma",
+      "file": "plugins/cordova-plugin-emma-sdk/www/emma.js",
+      "pluginId": "cordova-plugin-emma-sdk",
+      "clobbers": [
+        "window.plugins.EMMA"
+      ]
+    },
+    {
       "id": "cordova-plugin-ionic-webview.IonicWebView",
       "file": "plugins/cordova-plugin-ionic-webview/src/www/util.js",
       "pluginId": "cordova-plugin-ionic-webview",
@@ -15,18 +23,10 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "cordova.exec"
       ]
-    },
-    {
-      "id": "cordova-plugin-emma-sdk.emma",
-      "file": "plugins/cordova-plugin-emma-sdk/www/emma.js",
-      "pluginId": "cordova-plugin-emma-sdk",
-      "clobbers": [
-        "window.plugins.EMMA"
-      ]
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-ionic-webview": "5.0.0",
-    "cordova-plugin-emma-sdk": "1.5.0"
+    "cordova-plugin-emma-sdk": "1.5.0",
+    "cordova-plugin-ionic-webview": "5.0.0"
   };
 });
